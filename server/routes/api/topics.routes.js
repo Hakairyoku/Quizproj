8138af5abac8af2b41ad279b8333ac546208e0cb
Crollexx <crollexx@gmail.com>
2024-07-04 16:44:45 +0300

    createRoutesTopic

--- server/routes/api/topics.routes.js
+++ server/routes/api/topics.routes.js
@@ -1,1 +1,24 @@
-//
+const router = require('express').Router()
+const { Topics, Asks } = require('../../db/models')
+
+router.get('/', async (req, res) => {
+    try {
+        const topics = await Topics.findAll()
+        res.status(200).json({ message: 'success', topics })
+    } catch ({ message }) {
+        res.status(500).json({ error: message })
+    }
+});
+router.get('/:topicId/Asks', async (req, res) => {
+    try {
+        const { topicId } = req.params
+        const topic = await Asks.findAll({
+            where: { topicId: topicId }
+        })
+        res.status(200).json({ message: 'success', topic })
+    } catch ({ message }) {
+        res.status(500).json({ error: message })
+    }
+})
+
+module.exports = router
