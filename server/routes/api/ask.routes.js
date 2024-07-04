f2ca0f48ca16ecd56ead99709073f60b7e718bd9
Hakairyoku <nikischitov@gmail.com>
2024-07-04 17:24:00 +0300

    reworked

--- server/routes/api/ask.routes.js
+++ server/routes/api/ask.routes.js
@@ -1,8 +1,8 @@
 const router = require("express").Router();
-const { Asks } = require("../../db/models");
+const { Ask } = require("../../db/models");
 router.get("/", async (req, res) => {
   try {
-    const asks = await Asks.findAll();
+    const asks = await Ask.findAll();
     res.status(200).json({ message: "success", asks });
   } catch ({ message }) {
     res.status(500).json({ erroe: message });
@@ -13,7 +13,7 @@ router.get("/:id", async (req, res) => {
   try {
     const { id } = req.params;
     console.log("params", id);
-    const ask = await Asks.findBiPk(+id);
+    const ask = await Ask.findBiPk(+id);
     res.status(200).json({ message: "success", ask });
   } catch ({ message }) {
     res.status(500).json({ erroe: message });
